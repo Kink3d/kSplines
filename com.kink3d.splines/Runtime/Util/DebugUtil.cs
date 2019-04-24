48395b9ecc2d122cf1e54384c02651254315624c
Matt Dean <matthewdean3d@gmail.com>
2019-04-24 10:08:26 -0700

    Make DebugUtil public

--- com.kink3d.splines/Runtime/Util/DebugUtil.cs
+++ com.kink3d.splines/Runtime/Util/DebugUtil.cs
@@ -2,17 +2,17 @@
 
 namespace kTools.Splines
 {
-	internal static class DebugUtil
+	public static class DebugUtil
 	{
 #region Draw
-		internal static void DrawLine(Vector3 pointA, Vector3 pointB, DebugColors.DebugColor color)
+		public static void DrawLine(Vector3 pointA, Vector3 pointB, DebugColors.DebugColor color)
 		{
 			// Draw a line gizmo
 			Gizmos.color = color.wire;
 			Gizmos.DrawLine(pointA, pointB);
 		}
 
-		internal static void DrawHandle(Point point, Direction direction, DebugColors.DebugColor color)
+		public static void DrawHandle(Point point, Direction direction, DebugColors.DebugColor color)
 		{
 			// Draw a Handle gizmo
 			var endPoint = point.GetHandle(direction);
@@ -23,22 +23,22 @@ namespace kTools.Splines
 #endregion
 	}
 
-	internal static class DebugColors
+	public static class DebugColors
 	{
 #region Definitions
-		internal static DebugColor spline = new DebugColor(new Vector4(0.43f, 0.81f, 0.96f, 1.0f), new Vector4(0.43f, 0.81f, 0.96f, 0.25f));
-		internal static DebugColor handle = new DebugColor(new Vector4(0.99f, 0.82f, 0.64f, 1.0f), new Vector4(0.99f, 0.82f, 0.64f, 0.5f));
-		internal static DebugColor white = new DebugColor(new Vector4(1.00f, 1.00f, 1.00f, 1.0f), new Vector4(1.00f, 1.00f, 1.00f, 0.5f));
-		internal static DebugColor black = new DebugColor(new Vector4(0.00f, 0.00f, 0.00f, 1.0f), new Vector4(0.00f, 0.00f, 0.00f, 0.5f));
+		public static DebugColor spline = new DebugColor(new Vector4(0.43f, 0.81f, 0.96f, 1.0f), new Vector4(0.43f, 0.81f, 0.96f, 0.25f));
+		public static DebugColor handle = new DebugColor(new Vector4(0.99f, 0.82f, 0.64f, 1.0f), new Vector4(0.99f, 0.82f, 0.64f, 0.5f));
+		public static DebugColor white = new DebugColor(new Vector4(1.00f, 1.00f, 1.00f, 1.0f), new Vector4(1.00f, 1.00f, 1.00f, 0.5f));
+		public static DebugColor black = new DebugColor(new Vector4(0.00f, 0.00f, 0.00f, 1.0f), new Vector4(0.00f, 0.00f, 0.00f, 0.5f));
 #endregion
 
 #region Data Structures
-		internal struct DebugColor
+		public struct DebugColor
 		{
-			internal Vector4 wire;
-			internal Vector4 fill;
+			public Vector4 wire;
+			public Vector4 fill;
 
-			internal DebugColor(Vector4 wire, Vector4 fill)
+			public DebugColor(Vector4 wire, Vector4 fill)
 			{
 				// Define a new DebugColor
 				this.wire = wire;
