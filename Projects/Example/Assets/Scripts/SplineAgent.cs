d2e9a6bc1f6f00946a1d30b9ec02b9ae9f0411d4
Matt Dean <matthewdean3d@gmail.com>
2019-04-27 19:54:26 -0700

    Add Example project

---
+++ Projects/Example/Assets/Scripts/SplineAgent.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using UnityEngine;
+using kTools.Splines;
+
+public class SplineAgent : MonoBehaviour
+{
+#region Enumerations
+    public enum LoopMode
+    {
+        None,
+        Loop,
+        PingPong
+    }
+#endregion
+
+#region Properties
+    public Spline spline;
+    public float speed = 1.0f;
+    public LoopMode loopMode = LoopMode.None;
+    public bool playOnAwake = false;
+    public bool resetOnComplete = false;
+#endregion
+
+#region Data
+    private Direction m_Direction = Direction.Forward;
+#endregion
+
+#region Initialization
+    private void Awake()
+    {
+        // Play on Awake
+        if(playOnAwake)
+            Execute();
+    }
+#endregion
+
+#region Execution
+    /// <summary>
+    /// Evaluate the Agent along the Spline.
+    /// </summary>
+    public void Execute()
+    {
+        StartCoroutine(ExecutionEnumerator());
+    }
+
+    private IEnumerator ExecutionEnumerator()
+    {
+        // If theres no Spline dont calculate
+        if(spline == null)
+            yield break;
+        
+        // Move along Spline
+        float time = 0.0f;
+        while(time < 1.0f)
+        {
+            // Get t value
+            time += Time.deltaTime * speed;
+            float t = Mathf.Clamp(time, 0.0f, 1.0f);
+
+            // Set direction
+            if(m_Direction == Direction.Backward)
+                t = 1.0f - t;
+            
+            // Evaluate and position
+            bool looping = loopMode == LoopMode.Loop;
+            SplineValue value = spline.EvaluateWithSegmentLengths(t, looping);
+            transform.position = value.position;
+            yield return null;
+        }
+        
+        // If ping pong change direction
+        if(loopMode == LoopMode.PingPong)
+            m_Direction = m_Direction == Direction.Forward ? Direction.Backward : Direction.Forward;
+
+        // If looping continue
+        if(loopMode != LoopMode.None)
+            StartCoroutine(ExecutionEnumerator());
+
+        // If reset on complete
+        else if(resetOnComplete)
+        {
+            SplineValue value = spline.EvaluateWithSegmentLengths(0, false);
+            transform.position = value.position;
+        }
+    }
+#endregion
+}
---
+++ Projects/Example/Assets/Scripts/SplineRenderer.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using kTools.Splines;
+
+[ExecuteAlways]
+[RequireComponent(typeof(LineRenderer))]
+public class SplineRenderer : MonoBehaviour
+{
+#region Components
+    private LineRenderer m_LineRenderer;
+
+    public LineRenderer lineRenderer
+    {
+        get
+        {
+            if(m_LineRenderer == null)
+                m_LineRenderer = GetComponent<LineRenderer>();
+            return m_LineRenderer;
+        }
+    }
+#endregion
+
+#region Properties
+    public Spline spline;
+    public int segments = 64;
+#endregion
+
+#region Data
+    private Vector3[] m_Points;
+#endregion
+
+#region Update
+    [ExecuteInEditMode]
+    private void Update()
+    {
+        // If no Spline return
+        if(spline == null)
+            return;
+
+        // Render Spline
+        RenderSpline();
+    }
+#endregion
+
+#region Spline
+    private void RenderSpline()
+    {
+        // If no segments return
+        if(segments == 0)
+            return;
+
+        // Create points array
+        int pointCount = segments + 1;
+        m_Points = new Vector3[pointCount];
+
+        // Evaluate Spline
+        for(int i = 0; i < pointCount; i++)
+        {
+            float t = (float)i / (float)pointCount;
+            SplineValue value = spline.EvaluateWithSegmentLengths(t, false);
+            m_Points[i] = value.position;
+        }
+
+        // Set LineRenderer
+        lineRenderer.positionCount = pointCount;
+        lineRenderer.SetPositions(m_Points);
+    }
+#endregion
+}
